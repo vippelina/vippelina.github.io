0ef596abd672fd4b57c97060ff76c0ddaea06d45
Vibeke Tengroth <vibeketengroth@gmail.com>
2023-11-03 11:42:59 +0100

    made it vippekör

---
+++ src/app/components/Harmonies.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+export default function Harmonies() {
+  return (
+    <>
+      <h1>Stämmor vippes kör</h1>
+      <h2>Håll mitt hjärta</h2>
+      <h3>Sopran</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527135/hall_mitt_hjarta_harmonies/hall_mitt_hjarta_sopran_kjqg3i.mp3"
+      ></audio>
+      <h3>Alt</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527135/hall_mitt_hjarta_harmonies/hall_mitt_hjarta_alt_f2gvfx.mp3"
+      ></audio>
+      <h3>Tenor</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527135/hall_mitt_hjarta_harmonies/hall_mitt_hjarta_tenor_ej2ur4.mp3"
+      ></audio>
+      <h2>He will supply</h2>
+
+      <h3>Sopran</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527131/he_will_supply__harmonies/he_will_supply_sopran_g0riaw.mp3"
+      ></audio>
+      <h3>Alt</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527131/he_will_supply__harmonies/he_will_supply_alt_sfeczp.mp3"
+      ></audio>
+      <h3>Tenor</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527130/he_will_supply__harmonies/he_will_supply_tenor_jh6g7e.mp3"
+      ></audio>
+      <h2>Stand still</h2>
+      <h3>Sopran</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527126/stand_still__harmonies/stand_still_sopran_khsghh.mp3"
+      ></audio>
+      <h3>Alt</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527125/stand_still__harmonies/stand_still_alt_dtuavw.mp3"
+      ></audio>
+      <h3>Tenor</h3>
+      <audio
+        controls
+        src="https://res.cloudinary.com/dfmck30un/video/upload/v1698527126/stand_still__harmonies/stand_still_tenor_yputmm.mp3"
+      ></audio>
+    </>
+  );
+}
---
+++ src/app/components/Originals.tsx
@@ -0,0 +1,30 @@
+export default function Originals() {
+  return (
+    <>
+      <h2>Håll mitt hjärta - Björn Skifs</h2>
+      <iframe
+        src="https://www.youtube.com/embed/Ya9n4tr3PDU?si=9J7c2KnFxNLA5Ikn"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <h2>Stand still - The walls group</h2>
+      <iframe
+        src="https://www.youtube.com/embed/Se6Iv4FwO-E?si=xrNE7PAO9NdkDlyk"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <h2>He will supply - Kirk Franklin</h2>
+      <iframe
+        src="https://www.youtube.com/embed/GInruCmHiKc?si=rG1F10wcCq0Wytpr"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </>
+  );
+}
